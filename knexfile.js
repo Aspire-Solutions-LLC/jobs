ad6d6e6a03887c3679bc3f88a8e6ed9ebff22f2a
vanman9599 <vanjordan46@hotmail.com>
2020-02-20 08:49:01 -0800

    hardcoded Connection URL

--- knexfile.js
+++ knexfile.js
@@ -6,15 +6,11 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD, 
-      port: process.env.PORT
-    },
-    pool: {
-      min: 2,
-      max: 10
+      host: '127.0.0.1',
+      database: 'jobs',
+      user: 'van',
+      password: 'J3pxdam-7', 
+      port: 5432
     },
     migrations: {
       directory: "./data/migrations",
@@ -50,8 +46,8 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
-    connection: process.env.DATABASE_URL || {
+    client: "pg",
+    connection: 'postgres://aavzyrdgjgrcns:2aacd5adceb30911920f3d4d1c0084d940c4dd42ca6343c2b60ec06963e1a937@ec2-3-234-109-123.compute-1.amazonaws.com:5432/d5n04j114l20b2' || {
       host: process.env.DB_HOST,
       database: process.env.DB,
       user: process.env.DB_USER,
