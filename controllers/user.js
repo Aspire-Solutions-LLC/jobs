68a19ee366426eabbb1c03e3d9b11dbe623536b3
vanman9599 <vanjordan46@hotmail.com>
2020-02-19 22:15:46 -0800

    added 'engines' to package.json

--- controllers/user.js
+++ controllers/user.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router
   .route("/")
   .get(async (req, res) => {
-   // const users = await Users.find();
-   // res.status(200).json({ users });
+    const users = await Users.find();
+    res.status(200).json({ users });
    res.send("Welcome to Remote Jobs Board!");
   })
   .post(async (req, res) => {
