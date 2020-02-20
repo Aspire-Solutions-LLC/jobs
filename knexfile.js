cf727632f3903741c4e97ccacad61cde1ee902a3
vanman9599 <vanjordan46@hotmail.com>
2020-02-19 22:47:11 -0800

    desperate

--- knexfile.js
+++ knexfile.js
@@ -52,6 +52,7 @@ module.exports = {
   production: {
     client: "postgresql",
     connection: process.env.DATABASE_URL || {
+      host: process.env.DB_HOST,
       database: process.env.DB,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD
